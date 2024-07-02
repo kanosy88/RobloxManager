5d6cbc1dcec651188d3e1a7cf4e574e66558260b
kanosy88 <billyagbo38@gmail.com>
2024-07-02 21:10:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,96 @@
+<p align="center">
+  <a href="" rel="noopener">
+ <img width=200px height=200px src="https://i.imgur.com/O5EXdj2.jpeg" alt="Project logo"></a>
+</p>
+
+<h3 align="center">roblox-manager</h3>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+[![GitHub Issues](https://img.shields.io/github/issues/kylelobo/The-Documentation-Compendium.svg)](https://github.com/kylelobo/The-Documentation-Compendium/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/kylelobo/The-Documentation-Compendium.svg)](https://github.com/kylelobo/The-Documentation-Compendium/pulls)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](/LICENSE)
+
+</div>
+
+---
+
+<p align="center"> Roblox Manager is an app that lets you receive notifications when your friends change their status on Roblox.
+    <br>
+</p>
+
+## 📝 Table of Contents
+
+- [📝 Table of Contents](#-table-of-contents)
+- [🧐 About ](#-about-)
+- [🏁 Getting Started ](#-getting-started-)
+  - [Installing](#installing)
+- [🎈 Usage ](#-usage-)
+- [⛏️ Built Using ](#️-built-using-)
+- [✍️ Authors ](#️-authors-)
+- [🎉 Acknowledgements ](#-acknowledgements-)
+
+## 🧐 About <a name = "about"></a>
+
+Roblox Manager is an app that lets you receive notifications when your friends change their status on Roblox. Like Steam notifications.
+
+## 🏁 Getting Started <a name = "getting_started"></a>
+
+This application still under development, but a first 1.0 release is on the way very soon.
+
+For the moment, the only way to test the application is to download this github repo and perform the following instructions.
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running.
+
+To start cloning the GitHub repertoire on your PC
+
+```
+git clone repo
+```
+
+Then go to the file
+
+```
+cd ./repo
+```
+
+Install all dependence
+
+```
+npm install
+```
+
+Launch write this to launch this order to launch the application to test the application in its development environment
+
+```
+npm run dist
+```
+
+If you want to have an executable version of the application (.exe) write this
+
+```
+npm run dist
+```
+
+## 🎈 Usage <a name="usage"></a>
+
+If everything is well installed you should receive notification each time one of your friends changes status after launching the application.
+
+If this is not the case and you encounter an error. Please open a result.
+
+## ⛏️ Built Using <a name = "built_using"></a>
+
+- [Electron Template](https://github.com/maxstue/vite-reactts-electron-starter) - Template
+
+## ✍️ Authors <a name = "authors"></a>
+
+- [@kanosy](https://github.com/kanosy88) - Initial work
+
+See also the list of [contributors](https://github.com/kanosy88/RobloxManager/graphs/contributors) who participated in this project.
+
+## 🎉 Acknowledgements <a name = "acknowledgement"></a>
+
+- [@LxstCxn](https://github.com/kanosy88) - Idea
---
+++ src/App.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react';
+import AppBar from './AppBar';
+
+const NotificationIcon = './assets/icons/Icon-Electron.png';
+
+function App() {
+  console.log(window.ipcRenderer);
+
+  const [isOpen, setOpen] = useState(false);
+  const [isSent, setSent] = useState(false);
+  const [fromMain, setFromMain] = useState<string | null>(null);
+
+  const Notify = (title: string, body: string) => {
+    const notification_options: NotificationOptions = {
+      body: 'This is a Notification',
+      icon: NotificationIcon,
+      badge: 'https://developer.mozilla.org/en-US/docs/Web/API/Notification/badge',
+      tag: 'notification-sample'
+    };
+
+    Notification.requestPermission().then((result) => {
+      if (result === 'granted') return new Notification(title, notification_options);
+    });
+  };
+
+  const handleMinimize = () => {
+    if (window.Main) {
+      window.Main.Minimize();
+    } else {
+      console.log('You are in a Browser, so no Electron functions are available');
+    }
+  };
+
+  const handleToggle = () => {
+    if (isOpen) {
+      setOpen(false);
+      setSent(false);
+    } else {
+      setOpen(true);
+      setFromMain(null);
+    }
+  };
+  const sendMessageToElectron = () => {
+    if (window.Main) {
+      window.Main.sendMessage('Hello from React World');
+    } else {
+      setFromMain('You are in a Browser, so no Electron functions are available');
+    }
+    setSent(true);
+  };
+
+  useEffect(() => {
+    if (isSent && window.Main)
+      window.Main.on('message', (message: string) => {
+        setFromMain(message);
+      });
+  }, [fromMain, isSent]);
+
+  return (
+    <div className="flex flex-col h-screen">
+      {window.Main && (
+        <div className="flex-none">
+          <AppBar />
+        </div>
+      )}
+
+      <div className="flex-auto">
+        <div className=" flex flex-col justify-center items-center h-full bg-gray-800 space-y-4">
+          <h1 className="text-2xl text-gray-200">Vite + React + Typescript + Electron + Tailwind</h1>
+          <div className="flex flex-row gap-5">
+            <button
+              className="bg-yellow-400 py-2 px-4 rounded focus:outline-none shadow hover:bg-yellow-200"
+              onClick={handleToggle}
+            >
+              Click Me
+            </button>
+            <button
+              className="bg-yellow-400 py-2 px-4 rounded shadow hover:bg-yellow-200"
+              onClick={handleMinimize}
+              type="button"
+            >
+              Minimize
+            </button>
+            <button
+              onClick={() => Notify('Hello', 'This is a Notification')}
+              className="bg-yellow-400 py-2 px-4 rounded shadow hover:bg-yellow-200"
+              type="button"
+            >
+              Notify
+            </button>
+          </div>
+
+          {isOpen && (
+            <div className="flex flex-col space-y-4 items-center">
+              <div className="flex space-x-3">
+                <h1 className="text-xl text-gray-50">💝 Welcome 💝, now send a message to the Main 📩📩</h1>
+                <button
+                  onClick={sendMessageToElectron}
+                  className=" bg-green-400 rounded px-4 py-0 focus:outline-none hover:bg-green-300"
+                >
+                  Send
+                </button>
+              </div>
+              {isSent && (
+                <div>
+                  <h4 className=" text-green-500">Message sent!!</h4>
+                </div>
+              )}
+              {fromMain && (
+                <div>
+                  {' '}
+                  <h4 className=" text-yellow-200">{fromMain}</h4>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
